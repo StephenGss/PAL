cc9bd5567ca416314032d0a04320ec8d46f98f40
Stephen Goss <stephengs94@gmail.com>
2020-12-15 15:19:02 -0600

    enable speedup on server side

--- PolycraftAIGym/testSocket.py
+++ PolycraftAIGym/testSocket.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
- 
-import socket, random, time, json, os
- 
+
+import socket, random, time, json, os, datetime
+
 HOST = "127.0.0.1"
 PORT = 9000
 
@@ -11,50 +11,75 @@ run = True
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if 'PAL_PORT' in os.environ:
-	sock.connect((HOST, int(os.environ['PAL_PORT'])))
-	print('Using Port: ' + os.environ['PAL_PORT'])
+    sock.connect((HOST, int(os.environ['PAL_PORT'])))
+    print('Using Port: ' + os.environ['PAL_PORT'])
 else:
-	sock.connect((HOST, PORT))
-	print('Using Port: ' + str(PORT))
+    sock.connect((HOST, PORT))
+    print('Using Port: ' + str(PORT))
 
-while run:	# main loop
-	userInput = input()
-	if userInput == 'exit':	# wait for user input commands
-		run = False
-	elif userInput == 'wonder':	# testing automatic commands
-		count = 200
-		while count > 0:
-			sock.send(random.choice(movement))
-			sock.close()	# socket must be closed between after command
-			time.sleep(0.5)
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((HOST, PORT))
-			count = count - 1
-	else:
-		sock.send(str.encode(userInput + '\n'))
+while run:  # main loop
+    userInput = input()
+    if userInput == 'exit':  # wait for user input commands
+        run = False
+    elif userInput.startswith('speedtest'):  # testing automatic commands
+        command1 = 'MOVE w\n'
+        command2 = 'MOVE x\n'
+        if userInput.endswith('2'):
+            command1 = 'CRAFT 1 minecraft:planks minecraft:planks minecraft:planks minecraft:planks\n'
+            command2 = command1
+        if userInput.endswith('3'):
+            command1 = 'tp_to 2,4,6\n'
+            command2 = command1
+        totalCount = 100
+        count = totalCount
+        startTime = datetime.datetime.now()
+        while count > 0:
+            if count % 20 >= 10:
+                sock.send(str.encode(command1))
+            else:
+                sock.send(str.encode(command2))
+            # time.sleep(0.0001)
+            if True:
+                BUFF_SIZE = 4096  # 4 KiB
+                data = b''
+                while True:
+                    part = sock.recv(BUFF_SIZE)
+                    data += part
+                    if len(part) < BUFF_SIZE:
+                        # either 0 or end of data
+                        break
+            count = count - 1
+        endTime = datetime.datetime.now()
+        timeDiff = endTime - startTime
+        ms = (timeDiff.microseconds / 1000) + timeDiff.seconds * 1000
+        print("Time Taken: " + str(ms) + "ms")
+        print("FPS: " + str(totalCount / (ms / 1000)))
 
-	# Look for the response
-	amount_received = 0
-	amount_expected = 16
-	# if userInput.startswith('DATA') or userInput.startswith('LL'):	 # if we need to receive something, we have to listen for it. Maybe this should be a separate thread?
-	# 	data = ''
-	# 	data = sock.recv(10240).decode()
-	# 	data_dict = json.loads(data)
-	# 	print (data_dict)
-	# if not userInput.startswith('START'):
-	if True:
-		BUFF_SIZE = 4096  # 4 KiB
-		data = b''
-		while True:
-			part = sock.recv(BUFF_SIZE)
-			data += part
-			if len(part) < BUFF_SIZE:
-				# either 0 or end of data
-				break
-		print(data)
-		# data_dict = json.loads(data)
-		# print(data_dict)
+        sock.send(str.encode(userInput + '\n'))
+    else:
+        sock.send(str.encode(userInput + '\n'))
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = 16
+    # if userInput.startswith('DATA') or userInput.startswith('LL'):	 # if we need to receive something, we have to listen for it. Maybe this should be a separate thread?
+    # 	data = ''
+    # 	data = sock.recv(10240).decode()
+    # 	data_dict = json.loads(data)
+    # 	print (data_dict)
+    # if not userInput.startswith('START'):
+    if True:
+        BUFF_SIZE = 4096  # 4 KiB
+        data = b''
+        while True:
+            part = sock.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                # either 0 or end of data
+                break
+        print(data)
+# data_dict = json.loads(data)
+# print(data_dict)
 sock.close()
 
-
-print ("Socket closed")
+print("Socket closed")
