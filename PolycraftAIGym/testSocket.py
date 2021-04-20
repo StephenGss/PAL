cec3d9c9a65ef88289ff9a37cfa9b4ea05398c6c
Stephen Goss <stephengs94@gmail.com>
2021-04-20 14:21:38 -0500

    add image test

--- PolycraftAIGym/testSocket.py
+++ PolycraftAIGym/testSocket.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import socket, random, time, json, os, datetime
+import cv2
+import base64
+import numpy as np
 
-HOST = "127.0.0.1"
+HOST = "localhost"
 PORT = 9000
-
+last_result = ''
 movement = ['movenorth', 'movesouth', 'moveeast', 'movewest']
 
 run = True
@@ -21,6 +24,22 @@ while run:  # main loop
     userInput = input()
     if userInput == 'exit':  # wait for user input commands
         run = False
+    elif userInput == 'start_sample':
+        userInput = 'RESET domain ../pogo_100_PN/POGO_L00_T01_S01_X0100_U9999_V0_G00000_I0881_N0.json'
+        sock.send(str.encode(userInput + '\n'))
+    elif userInput.lower().startswith('show_screen'):
+        png_data_b64 = json.loads(data)['screen']['data']
+        data = base64.b64decode(png_data_b64)
+        # CV2
+        nparr = np.frombuffer(data, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
+        # CV
+        # img_ipl = cv2.CreateImageHeader((img_np.shape[1], img_np.shape[0]), cv.IPL_DEPTH_8U, 3)
+        # cv2.SetData(img_ipl, img_np.tostring(), img_np.dtype.itemsize * 3 * img_np.shape[1])
+        cv2.imshow("image", img_np)
+        cv2.waitKey(10000)
+        cv2.destroyAllWindows()
+        continue
     elif userInput.startswith('speedtest'):  # testing automatic commands
         command1 = 'MOVE w\n'
         command2 = 'MOVE x\n'
@@ -45,7 +64,7 @@ while run:  # main loop
                 while True:
                     part = sock.recv(BUFF_SIZE)
                     data += part
-                    if len(part) < BUFF_SIZE:
+                    if len(part) < BUFF_SIZE or part[-1] == 10:
                         # either 0 or end of data
                         break
             count = count - 1
@@ -74,10 +93,11 @@ while run:  # main loop
         while True:
             part = sock.recv(BUFF_SIZE)
             data += part
-            if len(part) < BUFF_SIZE:
+            if len(part) < BUFF_SIZE or part[-1] == 10:
                 # either 0 or end of data
                 break
         print(data)
+        last_result = data
 # data_dict = json.loads(data)
 # print(data_dict)
 sock.close()
