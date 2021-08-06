7402cab09879a082b90af06f202b39ed330acc49
Stephen Goss <stephengs94@gmail.com>
2021-08-06 12:45:46 -0500

    add fast instance loader function

--- PolycraftAIGym/testSocketTourn.py
+++ PolycraftAIGym/testSocketTourn.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
  
 import socket, random, time, json, os
+import tempfile
+import shutil
+import zipfile
  
 HOST = "127.0.0.1"
 PORT = 9005
@@ -18,8 +21,36 @@ else:
 	sock.connect((HOST, PORT))
 	print('Using Port: ' + str(PORT))
 
+dirpath = tempfile.mkdtemp()
+print(dirpath)
+
 while run:	# main loop
+	rand = random.Random
 	userInput = input()
+	if userInput.startswith('/'):
+		if userInput.startswith('/r'):
+			seed = random.randrange(999999)
+			print(str(seed))
+		else:
+			# TODO: check this seed. might lead to crashing 297220 and 501793
+			# check for adjacent chests 793405
+			seed = 180414
+		userInput = f'GENTOUR -c ../test -0 ../Novelty/input/huga_v2/huga_lvl_0.json -o ../Novelty/output/huga_test/ -f test -n 1 -w 0,0,1 -s {str(seed)} -R'
+		# userInput = f'GENTOUR -c ../test -0 ../Novelty/input/pogo_v2/pogo_lvl_0.json -o ../Novelty/output/pogo_test/ -f test -n 1 -w 0,0,1 -s {str(seed)} -R'
+	elif userInput.startswith('#'):
+		zipPath = "C:\\Users\\Stephen\\Polycraft World\\Polycraft World (Internal) - Documents\\05. SAIL-ON Program\\00. 06-12 Months\\98. 12M Tournament Files\\pogo-12M-tournaments-zipped\\POGO_100game_12M_full_evaluation_unknown_mode\\" \
+					+ userInput.split(" ")[1][0:16] + "\\" + userInput.split(" ")[1][17:22] + "\\" + userInput.split(" ")[1] + ".zip"
+		print(zipPath)
+		file = zipfile.ZipFile(zipPath)
+		file.extractall(path=dirpath)
+
+		filename = None
+		for f_name in os.listdir(dirpath):
+			if f_name.startswith(userInput.split(" ")[1][0:23] + userInput.split(" ")[1][25:33] + "_" + userInput.split(" ")[2]) and f_name.endswith('.json'):
+				filename = f_name
+		print(filename)
+		userInput = "Launch domain " + dirpath + "\\" + filename
+
 	if userInput == 'exit':	# wait for user input commands
 		run = False
 	elif userInput == 'wonder':	# testing automatic commands
@@ -57,5 +88,6 @@ while run:	# main loop
 		# print(data_dict)
 sock.close()
 
+shutil.rmtree(dirpath)
 
 print ("Socket closed")
