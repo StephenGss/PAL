89e099d0c8a29965f39e9aae6d6cb416b32c1bfc
Stephen Goss <stephengs94@gmail.com>
2021-11-19 09:25:05 -0600

    add show_map

--- PolycraftAIGym/testSocket.py
+++ PolycraftAIGym/testSocket.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
- 
-import socket, random, time, json, os
- 
-HOST = "127.0.0.1"
-PORT = 9000
 
+import socket, random, time, json, os, datetime, hashlib
+import cv2
+import base64
+import numpy as np
+from matplotlib import pyplot as plt
+
+HOST = "localhost"
+PORT = 9000
+last_result = ''
 movement = ['movenorth', 'movesouth', 'moveeast', 'movewest']
 
 run = True
@@ -17,20 +21,75 @@ else:
 	sock.connect((HOST, PORT))
 	print('Using Port: ' + str(PORT))
 
-while run:	# main loop
-	# time.sleep(0.5)
+print('Special commands: "start_sample", "show_screen" (after a sense_screen)')
+while run:  # main loop
 	userInput = input()
-	if userInput == 'exit':	# wait for user input commands
+	if userInput == 'exit':  # wait for user input commands
 		run = False
-	elif userInput == 'wonder':	# testing automatic commands
-		count = 200
+	elif userInput == 'start_sample':
+		userInput = 'RESET domain ../pogo_100_PN/POGO_L00_T01_S01_X0100_U9999_V0_G00000_I0881_N0.json'
+		sock.send(str.encode(userInput + '\n'))
+	elif userInput.lower().startswith('show_screen'):
+		png_data_b64 = json.loads(data)['screen']['data']
+		data = base64.b64decode(png_data_b64)
+		# CV2
+		nparr = np.frombuffer(data, np.uint8)
+		img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
+		cv2.imshow("image", img_np)
+		cv2.waitKey(10000)
+		cv2.destroyAllWindows()
+		continue
+	elif userInput.startswith('show_map'):
+		map_data = json.loads(data)['map']
+		imgdata = np.zeros((map_data["size"][0], map_data["size"][2], 3))
+		blocks = map_data['blocks']
+		for x in range(map_data["size"][0]):
+			for y in range(map_data["size"][2]):
+				hash = int(hashlib.sha1(blocks[y*map_data["size"][0] + x][10:].encode("utf-8")).hexdigest(), 16)
+				imgdata[x][y][0] = ((hash & 0x00FF0000) >> 16) / 256.0
+				imgdata[x][y][1] = ((hash & 0x0000FF00) >> 8) / 256.0
+				imgdata[x][y][2] = (hash & 0x000000FF) / 256.0
+
+		print(str(map_data["size"][0]))
+		print(str(map_data["size"][2]))
+		plt.imshow(imgdata, interpolation='nearest')
+		plt.show()
+		continue
+	elif userInput.startswith('speedtest'):  # testing automatic commands
+		command1 = 'MOVE w\n'
+		command2 = 'MOVE x\n'
+		if userInput.endswith('2'):
+			command1 = 'CRAFT 1 minecraft:planks minecraft:planks minecraft:planks minecraft:planks\n'
+			command2 = command1
+		if userInput.endswith('3'):
+			command1 = 'tp_to 2,4,6\n'
+			command2 = command1
+		totalCount = 100
+		count = totalCount
+		startTime = datetime.datetime.now()
 		while count > 0:
-			sock.send(random.choice(movement))
-			sock.close()	# socket must be closed between after command
-			# time.sleep(0.5)
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((HOST, PORT))
+			if count % 20 >= 10:
+				sock.send(str.encode(command1))
+			else:
+				sock.send(str.encode(command2))
+			# time.sleep(0.0001)
+			if True:
+				BUFF_SIZE = 4096  # 4 KiB
+				data = b''
+				while True:
+					part = sock.recv(BUFF_SIZE)
+					data += part
+					if len(part) < BUFF_SIZE or part[-1] == 10:
+						# either 0 or end of data
+						break
 			count = count - 1
+		endTime = datetime.datetime.now()
+		timeDiff = endTime - startTime
+		ms = (timeDiff.microseconds / 1000) + timeDiff.seconds * 1000
+		print("Time Taken: " + str(ms) + "ms")
+		print("FPS: " + str(totalCount / (ms / 1000)))
+
+		sock.send(str.encode(userInput + '\n'))
 	else:
 		sock.send(str.encode(userInput + '\n'))
 
@@ -49,13 +108,13 @@ while run:	# main loop
 		while True:
 			part = sock.recv(BUFF_SIZE)
 			data += part
-			if len(part) < BUFF_SIZE:
+			if len(part) < BUFF_SIZE or part[-1] == 10:
 				# either 0 or end of data
 				break
 		print(data)
-		# data_dict = json.loads(data)
-		# print(data_dict)
+		last_result = data
+# data_dict = json.loads(data)
+# print(data_dict)
 sock.close()
 
-
-print ("Socket closed")
+print("Socket closed")
