57b86d929ffa9c2ce0f05b11d6c0016f68ffcf71
Stephen Goss <stephengs94@gmail.com>
2022-02-09 13:43:31 -0600

    updates

--- PolycraftAIGym/testSocketTourn.py
+++ PolycraftAIGym/testSocketTourn.py
@@ -35,8 +35,8 @@ while run:  # main loop
         else:
             # TODO: check this seed. might lead to crashing 297220 and 501793
             # check for adjacent chests 793405
-            # 619545
-            seed = 258235
+            # 619545, 258235, 207195
+            seed = 997270
         difficulty_weight = "1,0,0"
         if "-dh" in userInput:
             difficulty_weight = "0,0,1"
@@ -53,8 +53,11 @@ while run:  # main loop
 
         # userInput = f'GENTOUR -c ../test -0 ../Novelty/input/huga_v2/huga_lvl_0_demo.json -o ../Novelty/output/huga_test/ -f test -n 1 -w 0,0,1 -s {str(seed)} -R'
         # userInput = f'GENTOUR -c ../test -0 ../Novelty/input/pogo_v2/pogo_lvl_0.json -o ../Novelty/output/pogo_test/ -f test -n 1 -w 0,0,1 -s {str(seed)} -R'
+        # POGO v2 **************
         # userInput = f'GENTOUR -c ../test -0 ../Novelty/input/pogo_v2/pogo_{str(n_level)}-{str(n_type)}-{str(n_stype)}' \
         #             f'.json -o ../Novelty/output/pogo_test/ -f test -n 1 -w {difficulty_weight} -s {str(seed)} -R'
+        # **********************
+        # HUGA
         userInput = f'GENTOUR -c ../test -0 ../Novelty/input/huga_v2/huga_{str(n_level)}-{str(n_type)}-{str(n_stype)}' \
                     f'.json -o ../Novelty/output/huga_test/ -f test -n 1 -w {difficulty_weight} -s {str(seed)} -R'
         # old HUGA
